294cfdf5018ae2a7f56c33c3315381806247ddc4
666 MEMENTO <queen1448@nate.com>
2022-06-24 14:39:19 +0900

    save

--- js/eventHandler.js
+++ js/eventHandler.js
@@ -7,6 +7,8 @@ export const eventHandler = () => {
   const hamTop = document.getElementById("hamTop");
   const hamMid = document.getElementById("hamMid")
   const hamBottom = document.getElementById("hamBottom")
+  const fontY = document.getElementById("fontY");
+  const fontS = document.getElementById("fontS");
 
 
 
@@ -14,15 +16,27 @@ export const eventHandler = () => {
     
 hamBtn.addEventListener("click", ()=>{
   hamTop.style.transform = "rotate(-45deg)";
-  hamTop.style.marginTop = 24.5+"px";
+  hamTop.style.marginTop = 24.7+"px";
   hamMid.style.opacity = 0;
   hamBottom.style.transform = "rotate(45deg)";
   hamBottom.style.marginBottom = 25+"px";
-  hamBottom.style.transition = "0.4s";
-  hamTop.style.transition = "0.4s";
-  hamMid.style.transition = "2s";
-  
+  hamBottom.style.transition = "0.6s";
+  hamTop.style.transition = "0.6s";
+  hamMid.style.transition = "2.2s";
 })
+
+setTimeout(function (){
+  fontS.style.opacity = 0.8;
+  fontS.style.transition = "2s";
+},1500);
+
+setTimeout(function (){
+  fontY.style.opacity = 0.9;
+  fontY.style.transition = "2s";
+},2000);
+
+
+
 
 
 
